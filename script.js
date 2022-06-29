35829368c6180797669b832f6921f41a5b88ce78
Luciano Carvalho <carvalholuciano232@gmail.com>
2022-06-29 14:10:05 -0300

    atualização de arquivos

--- script.js
+++ script.js
@@ -2,15 +2,37 @@ let calcular = ()=>{
  let nome = document.getElementById("nome").value;
  let altura = document.getElementById("altura").value;
  let peso = document.getElementById("peso").value;
- let campo = document.getElementById("pcampo");
+ let campo = document.getElementById("pcampo").value;
 
  let imc;
  let text;
+ let contentCampo;
  imc = peso / (altura * altura);
 
- //text = `ola ${nome} seu imc é imc`;
-campo.innerHTML = imc.toString;
+ sessionStorage.setItem("valorIMC",imc);
+ contentCampo = JSON.parse(sessionStorage.getItem("valorIMC"));
 
+ if(contentCampo < 18.5){
+
+    campo.innerHTML = contentCampo;
+    text = "Você está magro";
+
+}else if (contentCampo < 24.9){
+
+    text = "Você está normal";
+
+} else if(contentCampo < 29.9){
+   
+    text = "Você está com sobrepeso";
+
+}else if (contentCampo < 39.9){
+
+    text = "Você está com obesidade";
+
+}else if(contentCampo > 39.9){
+    
+    text = "Você está com obesidade mórbida";
+}
 
 }
 
