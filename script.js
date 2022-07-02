cd42a6b247d670b768d10ea0a1eb57f89cc0a307
Luciano <carvalholuciano232@gmail.com>
2022-07-02 10:06:59 -0300

    terminei a parte do script

--- script.js
+++ script.js
@@ -1,40 +1,19 @@
-let calcular = ()=>{
- let nome = document.getElementById("nome").value;
+function calcular(){
+
  let altura = document.getElementById("altura").value;
  let peso = document.getElementById("peso").value;
- let campo = document.getElementById("pcampo").value;
+ let nome = document.getElementById("nome").value;
+ let imc = peso / (altura * altura);
 
- let imc;
- let text;
- let contentCampo;
- imc = peso / (altura * altura);
 
- sessionStorage.setItem("valorIMC",imc);
- contentCampo = JSON.parse(sessionStorage.getItem("valorIMC"));
+ sessionStorage.setItem("nome",nome)
+ sessionStorage.setItem("imc",imc)
+ 
 
- if(contentCampo < 18.5){
-
-    campo.innerHTML = contentCampo;
-    text = "Você está magro";
-
-}else if (contentCampo < 24.9){
-
-    text = "Você está normal";
-
-} else if(contentCampo < 29.9){
-   
-    text = "Você está com sobrepeso";
-
-}else if (contentCampo < 39.9){
-
-    text = "Você está com obesidade";
-
-}else if(contentCampo > 39.9){
-    
-    text = "Você está com obesidade mórbida";
-}
 
 }
+
+
 
 
 
---
+++ script2.js
@@ -0,0 +1,30 @@
+let campo = document.getElementById("pcampo");
+let text = document.getElementById("texto");
+
+let nomesession = sessionStorage.getItem("nome");
+let imcsession = sessionStorage.getItem("imc");
+
+if(imcsession < 18.5 ){
+    campo.innerHTML = ` Olá ${nomesession} Seu IMC é ${parseFloat(imcsession)}`;
+    text.innerHTML = "Você está Magro(a)"
+} else if(imcsession < 24.9){
+    campo.innerHTML = ` Olá ${nomesession} Seu IMC é ${parseFloat(imcsession)}`;
+    text.innerHTML = "Você está Normal)"
+}else if (imcsession < 29.9){
+    campo.innerHTML = ` Olá ${nomesession} Seu IMC é ${parseFloat(imcsession)}`;
+    text.innerHTML = "Você está com sobrepeso"
+}else if(imcsession < 39.9){
+    campo.innerHTML = ` Olá ${nomesession} Seu IMC é ${parseFloat(imcsession)}`;
+    text.innerHTML = "Você está com obesidade"
+}else if (imcsession > 39.9){
+    campo.innerHTML = ` Olá ${nomesession} Seu IMC é ${parseFloat(imcsession)}`;
+    text.innerHTML = "Você está com obesidade mórbida"
+}
+
+function reset(){
+
+    sessionStorage.clear();
+}
+
+
+
