45fc59d871e537c46263b68ce9f1a1d0c1b3705e
Luciano <carvalholuciano232@gmail.com>
2022-07-02 12:59:38 -0300

    arredondei os valores do resultado do IMC

--- script2.js
+++ script2.js
@@ -2,7 +2,7 @@ let campo = document.getElementById("pcampo");
 let text = document.getElementById("texto");
 
 let nomesession = sessionStorage.getItem("nome");
-let imcsession = parseFloat (sessionStorage.getItem("imc"));
+let imcsession = parseFloat (sessionStorage.getItem("imc")).toFixed(2);
 
 if(imcsession < 18.5 ){
     campo.innerHTML = ` Olá ${nomesession} Seu IMC é ${parseFloat(imcsession)}`;
