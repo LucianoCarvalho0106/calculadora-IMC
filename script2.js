ed673604fdc8c7283ae60092279c70bb35580bde
Luciano <carvalholuciano232@gmail.com>
2022-07-02 10:32:47 -0300

    terminei o css

--- script2.js
+++ script2.js
@@ -2,14 +2,14 @@ let campo = document.getElementById("pcampo");
 let text = document.getElementById("texto");
 
 let nomesession = sessionStorage.getItem("nome");
-let imcsession = sessionStorage.getItem("imc");
+let imcsession = parseFloat (sessionStorage.getItem("imc"));
 
 if(imcsession < 18.5 ){
     campo.innerHTML = ` Olá ${nomesession} Seu IMC é ${parseFloat(imcsession)}`;
     text.innerHTML = "Você está Magro(a)"
 } else if(imcsession < 24.9){
     campo.innerHTML = ` Olá ${nomesession} Seu IMC é ${parseFloat(imcsession)}`;
-    text.innerHTML = "Você está Normal)"
+    text.innerHTML = "Você está Normal"
 }else if (imcsession < 29.9){
     campo.innerHTML = ` Olá ${nomesession} Seu IMC é ${parseFloat(imcsession)}`;
     text.innerHTML = "Você está com sobrepeso"
